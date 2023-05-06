0610504b60e90ada6237d035ddf6ec2b16f7f426
Zyruks <kuzenshi@gmail.com>
2023-05-06 13:19:49 -0300

    chore(components): :fire: remove className prop from Counter component and unnecessary comments
    Refactored Counter component to remove the className prop, as it was not being used.
    
    The component now uses a default CSS class for styling. This change improves the code's readability and simplifies the component's.

--- apps/nextjs/src/components/Counter/Counter.tsx
+++ apps/nextjs/src/components/Counter/Counter.tsx
@@ -1,6 +1,4 @@
-import { setTimeout } from 'timers';
 import { useEffect, useState } from 'react';
-import cn from 'classnames';
 
 interface CounterProps {
   /**
@@ -21,48 +19,26 @@ interface CounterProps {
   className?: string;
 }
 
-export const Counter = ({ className, start, end, speed = 100 }: CounterProps) => {
+export const Counter = ({ start, end, speed = 100 }: CounterProps) => {
   const [count, setCount] = useState(start);
-  const classes = {
-    container: cn(className, 'counter min-w-[30px]'),
-    child: cn('count'),
-  };
 
   useEffect(() => {
-    /**
-     * If the end value changes, reset the count to the start value
-     */
-
     setCount(start);
   }, [start]);
 
   useEffect(() => {
-    /**
-     * Use a timeout to increment the count every 'speed' milliseconds
-     * until the end value is reached
-     */
-
     const timeoutId = setTimeout(() => {
       if (count < end) {
         setCount(count + 1);
       }
     }, speed);
 
-    /**
-     * Clean up the timeout when the component unmounts
-     * or when the end value changes
-     */
-
     return () => clearTimeout(timeoutId);
   }, [count, end, speed]);
 
-  /**
-   * Return an empty div for the specified delay before starting the counter
-   */
-
   return (
-    <div className={classes.container}>
-      <span className={classes.child}>{count}</span>
+    <div className="counter min-w-[30px]">
+      <span>{count}</span>
     </div>
   );
 };
